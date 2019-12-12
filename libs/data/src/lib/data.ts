3f7520f6324413dd5fda3dbd78af538dcbb6a11c
asus <haouari.rabeb@gmail.com>
2019-12-12 14:21:49 +0100

    database include

--- libs/data/src/lib/data.ts
+++ libs/data/src/lib/data.ts
@@ -1,3 +1,69 @@
 export interface Todo {
   title: string;
 }
+//the methods here are supposed to be in services.. I just put em to not forget anything ^^
+export class Product {
+  id: number;
+  name: string;
+  owner: string;
+  category: string;
+  description: string;
+  nbrComments: number;
+  nbrRatings: number;
+  rate: number;
+  path: string;
+  moyenne(){
+    this.rate=this.nbrRatings/5;
+  }
+}
+
+abstract class User {
+  id: number;
+  email: string;
+  mdp: string;
+  numb: number;
+  auth(){
+
+  }
+  register(){
+
+  }
+  editprofile(){
+
+  }
+}
+
+export class Tester extends User{
+  name: string;
+  firstName: string;
+  age: number;
+  sexe: string;
+  userName: string;
+  path: string;
+  addComment(){
+
+  }
+  rate(){
+
+  }
+}
+
+export class ProductOwner extends User{
+  orgNAme: string;
+  sector: string;
+  path: string;
+  addProduct(){
+
+  }
+  deleteProduct(){
+
+  }
+  editProduct(){
+
+  }
+}
+
+export class Comment {
+  productId: number;
+  testerId: number;
+}
