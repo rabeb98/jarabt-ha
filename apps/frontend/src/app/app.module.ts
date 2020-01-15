650ad5a6d02f959b415caa2f96d8eac0f5b0b026
ayaBoughzala <ayaboughzala@hotmail.com>
2020-01-15 17:09:14 +0100

    commit n3)

--- apps/frontend/src/app/app.module.ts
+++ apps/frontend/src/app/app.module.ts
@@ -6,16 +6,18 @@ import { RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import {FormsModule} from '@angular/forms';
 import { HomeComponent } from './home/home.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RatingComponent } from './rating/rating.component'
+import {StarRatingModule} from 'angular-star-rating';
 @NgModule({
-  declarations: [AppComponent, LoginComponent, HomeComponent,],
+  declarations: [AppComponent, LoginComponent, HomeComponent, RatingComponent,],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
     NgbModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabled' })
+    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    StarRatingModule.forRoot() ,
   ],
   providers: [],
   bootstrap: [AppComponent]
